4b4bb33cb63dee2d211206a41cb669f0853f314a
du2lee <dh151446@gmail.com>
2021-05-09 10:23:11 +0900

    nothing

--- 1931.py
+++ 1931.py
@@ -1,6 +1,28 @@
 import sys
+
 N = int(sys.stdin.readline())
 list_time = []
+
+
+
+
+for i in range(N):
+    line = [0, 0]
+    line[0], line[1] =  map(int, sys.stdin.readline().split())
+    list_time.append(line)
+
+list_time.sort(key=lambda x:(x[1], x[0]))
+finish = list_time[0][1]
+count = 1
+
+for k in range(1, N):
+    if finish <= list_time[k][0]:
+        count += 1
+        finish = list_time[k][1]     
+print(list_time)  
+print(count)
+
+
 
 #for i in range(N):
 #    line = []              # 안쪽 리스트로 사용할 빈 리스트 생성
@@ -10,12 +32,6 @@ list_time = []
 
 #for j in range(N):
 #    list_time[j][0],  list_time[j][1] = map(int, sys.stdin.readline().split())
-
-for i in range(N):
-    line = [0, 0]
-    line[0], line[1] =  map(int, sys.stdin.readline().split())
-    list_time.append(line)
-
 
 
 #list_time.sort(key=lambda x:(x[0], x[1])) ##알고리즘 빅오가 크다.
@@ -32,14 +48,3 @@ for i in range(N):
 #            finish = list_time[l][1]
 #    if max < count:
 #        max = count
-
-list_time.sort(key=lambda x:(x[1], x[0]))
-finish = list_time[0][1]
-count = 1
-
-for k in range(1, N):
-    if finish <= list_time[k][0]:
-        count += 1
-        finish = list_time[k][1]     
-print(list_time)  
-print(count)
