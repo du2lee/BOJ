1ea1fba38ed82c1cf3c9a1326ab45b0b5e1b73b2
du2lee <dh151446@gmail.com>
2021-05-09 17:47:14 +0900

    update updownleftright

--- updownleftright.py
+++ updownleftright.py
@@ -6,12 +6,12 @@ x, y = 0 , 0
 
 list_move = sys.stdin.readline().split()
 move_type = ['U', 'D', 'L', 'R']
-move_direction = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+move_direction = [[0, -1], [0, 1], [-1, 0], [1, 0]]
 
-for i in range(N):
+for i in range(len(list_move)):
     for j in range(len(move_type)): #if문 단축
         if (list_move[i] == move_type[j]):
-            if (j == 0 and x == 0) or (j == 2 and y == 0):
+            if (j == 0 and y <= 0) or (j == 2 and x <= 0) or (j == 3 and x >= N) or (j == 1 and y >= N):
                 pass
             else:
                 x += move_direction[j][0]
