1d8ffdf93af0d19ec9465aba403e8b669010c4cf
du2lee <dh151446@gmail.com>
2021-06-11 19:40:11 +0900

    function

--- 4673.py
+++ 4673.py
@@ -1,21 +1,12 @@
-def d():
-    list_self = [0 for i in range(10001)]
-    list_self[0] = 1
-    cache = 0
-    for i in range(1,10001):
-        if i < 10:
-            cache = i+i
-        elif i <= 100:
-            cache = i+(i//10)+(i%10)
-        elif i <=1000:
-            cache = i+(i//100)+((i//10)%10)+(i%10)
-        elif i <=10000:
-            cache = i+(i%10)+((i//10)%10) + ((i//100)%10) + (i//1000)
-        if cache <= 10000:
-            list_self[cache] = 1
+array = set(range(1,10001))
+selfNum_array = set()
 
-    for j in range(10000):
-        if list_self[j] == 0:
-            print(j)
+for i in range(1, 10001):
+    for j in str(i):
+        i += int(j)
+    selfNum_array.add(i)
 
-d()
+selfNum = sorted(array - selfNum_array)
+
+for i in selfNum:
+    print(i)
