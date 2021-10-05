a358b0572b5d74f24e73990a2f69bcd37d3b1e8b
duhui <dh151446@gmail.com>
2021-10-05 15:19:41 +0900

    211005

--- 1783.py
+++ 1783.py
@@ -4,6 +4,8 @@ N ,M = map(int,sys.stdin.readline().split())
 
 if N == 1 or M == 1:
     print(1)
+# elif M == 2:
+#     print(min(4, int((N - 1) / 2) + 1))           //이거 때문에 엄청 틀림
 elif N == 2:
     print(min(4, int((M - 1) / 2) + 1))
 elif M < 7:
