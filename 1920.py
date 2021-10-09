0034d5c782c770c064e3fc23bd87b626e90f300d
duhui <dh151446@gmail.com>
2021-10-09 23:44:25 +0900

    211009

--- 1920.py
+++ 1920.py
@@ -31,3 +31,9 @@ N = int(input())
 A = set(map(int,input().split()))
 M = int(input())
 B = list(map(int,input().split()))
+
+for idx in B:
+    if idx not in A:
+        print(0)
+    else:
+        print(1)
