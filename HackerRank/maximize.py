82c8ce2223b39980442f526a9df9293bc44bbc8f
duhui <dh151446@gmail.com>
2022-02-06 19:58:32 +0900

    20220206

--- HackerRank/maximize.py
+++ HackerRank/maximize.py
@@ -4,7 +4,8 @@ K, M = map(int, input().split())
 N = []
 
 for _ in range(K):
-    N.append(list(map(int, input().split()))[1:])   
+    N.append(list(map(int, input().split()))[1:])
+
 MAX = -1
 for i in product(*N):
     MAX = max(sum(map(lambda x: x**2, i))%M, MAX)
