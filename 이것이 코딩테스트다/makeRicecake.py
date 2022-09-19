59429447b9cd0fe6925458f539e41e1ecdd25fd5
duhui <dh151446@gmail.com>
2022-09-19 18:46:01 +0900

    이진탐색 다시풀어봄

--- 이것이 코딩테스트다/makeRicecake.py
+++ 이것이 코딩테스트다/makeRicecake.py
@@ -1,25 +1,57 @@
-import sys
+N, M = list(map(int, input().split()))
 
-N, M = list(map(int, sys.stdin.readline().rstrip().split()))
-ricecakes = list(map(int, sys.stdin.readline().rstrip().split()))
-
-ricecakes.sort(reverse=True)
+array = list(map(int, input().split()))
+array.sort(reverse=True)
 
 start = 0
-end = ricecakes[0]
-result = 0
+end = array[0]
+result = -1
 
 while start <= end:
     mid = (start + end) // 2
-    total = 0
-    for i in ricecakes:
-        if i > mid:
-            total += i - mid
+    cache = 0
+    for i in array:
+        if i <= mid:
+            break
+        cache += i - mid
     
-    if total < M:
+    if cache < M:
         end = mid - 1
     else:
         result = mid
         start = mid + 1
-
 print(result)
+
+    
+
+
+
+
+
+
+
+# import sys
+
+# N, M = list(map(int, sys.stdin.readline().rstrip().split()))
+# ricecakes = list(map(int, sys.stdin.readline().rstrip().split()))
+
+# ricecakes.sort(reverse=True)
+
+# start = 0
+# end = ricecakes[0]
+# result = 0
+
+# while start <= end:
+#     mid = (start + end) // 2
+#     total = 0
+#     for i in ricecakes:
+#         if i > mid:
+#             total += i - mid
+    
+#     if total < M:
+#         end = mid - 1
+#     else:
+#         result = mid
+#         start = mid + 1
+
+# print(result)
--- 이것이 코딩테스트다/searchPart.py
+++ 이것이 코딩테스트다/searchPart.py
@@ -1,25 +1,59 @@
 import sys
 
-def binarySearch(arr, start, end, value):
+def binarySearch(array, start, end, value):
     while start < end:
         mid = (start + end) // 2
-        if arr[mid] == value:
-            return mid
-        elif value < arr[mid]:
+        if array[mid] == value:
+            return True
+        elif array[mid] > value:
             end = mid - 1
         else:
             start = mid + 1
-    return None
+    return False
 
-N = int(sys.stdin.readline().rstrip())
-arrayN = list(map(int,sys.stdin.readline().rstrip().split()))
-M = int(sys.stdin.readline().rstrip())
-arrayM = list(map(int, sys.stdin.readline().rstrip().split()))
 
-arrayN.sort()
-for i in arrayM:
-    cache = binarySearch(arrayN, 0, N, i)
-    if cache == None:
-        print('No', end = ' ')
+N = int(sys.stdin.readline())
+
+array = list(map(int, sys.stdin.readline().rstrip().split()))
+
+M = int(sys.stdin.readline())
+
+searchArr = list(map(int, sys.stdin.readline().rstrip().split()))
+
+array.sort()
+for i in searchArr:
+    if binarySearch(array, 0, N, i):
+        print('yes')
     else:
-        print('yes', end = ' ')
+        print('no')
+
+
+
+
+
+
+# import sys
+
+# def binarySearch(arr, start, end, value):
+#     while start < end:
+#         mid = (start + end) // 2
+#         if arr[mid] == value:
+#             return mid
+#         elif value < arr[mid]:
+#             end = mid - 1
+#         else:
+#             start = mid + 1
+#     return None
+
+# N = int(sys.stdin.readline().rstrip())
+# arrayN = list(map(int,sys.stdin.readline().rstrip().split()))
+# M = int(sys.stdin.readline().rstrip())
+# arrayM = list(map(int, sys.stdin.readline().rstrip().split()))
+
+# arrayN.sort()
+# for i in arrayM:
+#     cache = binarySearch(arrayN, 0, N, i)
+#     if cache == None:
+#         print('No', end = ' ')
+#     else:
+#         print('yes', end = ' ')
