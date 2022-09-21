f6dfe82686630f1a400040cb6e7296b4fc41c849
duhui <dh151446@gmail.com>
2022-09-21 10:19:14 +0900

    브루트포스 복습

--- BOJ/python/6603.py
+++ BOJ/python/6603.py
@@ -1,25 +1,59 @@
-import sys
 from collections import deque
+result = deque()
 
-cache_array = deque()
-
-def func(n):
-
-    if len(cache_array) == 6:
-        print(*cache_array)
+def func(N):
+    if len(result) == 6:
+        print(*result)
         return
 
-    for i in range(n, len(array)):
-        if array[i] not in cache_array:
-            cache_array.append(array[i])
+    for i in range(N, len(arr)):
+        if arr[i] not in result:
+            result.append(arr[i])
             func(i + 1)
-            cache_array.pop()
+            result.pop()
+
 
 while True:
-    array = deque(map(int, sys.stdin.readline().split()))
-
-    if array[0] == 0 and len(array) == 1:
+    arr = deque(map(int, input().split()))
+    if arr[0] == 0:
         break
-    N = array.popleft()
+    arr.popleft()
     func(0)
     print('')
+
+
+
+
+
+
+
+
+
+
+
+
+# import sys
+# from collections import deque
+
+# cache_array = deque()
+
+# def func(n):
+
+#     if len(cache_array) == 6:
+#         print(*cache_array)
+#         return
+
+#     for i in range(n, len(array)):
+#         if array[i] not in cache_array:
+#             cache_array.append(array[i])
+#             func(i + 1)
+#             cache_array.pop()
+
+# while True:
+#     array = deque(map(int, sys.stdin.readline().split()))
+
+#     if array[0] == 0 and len(array) == 1:
+#         break
+#     N = array.popleft()
+#     func(0)
+#     print('')
