b06868474a40630f174b28172c1908119e047429
duhui <dh151446@gmail.com>
2022-09-27 12:40:43 +0900

    그리디 복습

--- BOJ/python/13305.py
+++ BOJ/python/13305.py
@@ -1,13 +1,43 @@
 N = int(input())
-lines = list(map(int, input().split()))
-gas = list(map(int, input().split()))
 
-answer = 0
-minNum = 1000000000
+gas = [0] + list(map(int,input().split()))
+nodes = list(map(int, input().split()))
 
-for i in range(N - 1):
-    if gas[i] < minNum:
-        minNum = gas[i]
-    answer += minNum * lines[i]
+dp = [0] * N
+min = 10001
 
-print(answer)
+for i in range(1, N):
+    if nodes[i - 1] < min:
+        min = nodes[i - 1]
+
+    dp[i] += dp[i - 1] + min * gas[i]
+
+print(dp[-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# N = int(input())
+# lines = list(map(int, input().split()))
+# gas = list(map(int, input().split()))
+
+# answer = 0
+# minNum = 1000000000
+
+# for i in range(N - 1):
+#     if gas[i] < minNum:
+#         minNum = gas[i]
+#     answer += minNum * lines[i]
+
+# print(answer)
